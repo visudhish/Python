72f0c851690713c9a9baeb5f39e8c05817b024e2
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-06 08:19:47 +0800

    added spaces

--- GreatestAmongThree.py
+++ GreatestAmongThree.py
@@ -1,16 +1,16 @@
-number1=input("number 1")
+number1=input("number 1 : ")
 int1=int(number1)
-number2=input("number2")
+number2=input("number2 : ")
 int2=int(number2)
-number3=input("number3")
+number3=input("number3 : ")
 int3=int(number3)
 if(int1>int2):
    greaternumber=int1
 else:
    greaternumber=int2
 if(greaternumber>int3):
-   print(greaternumber,"is the greatest among three")
+   print(greaternumber," is the greatest among three")
 else:
-   print(int3,"isthe greatest among three")   
+   print(int3," isthe greatest among three")   
 
       
