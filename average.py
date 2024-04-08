3a7c416d2de3f68e165a0191c4fb8125653dac5a
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-08 19:37:03 +0800

    area.py

--- average.py
+++ average.py
@@ -1,12 +1,23 @@
-#initilisation
-Geometry=input("geometrymarks:")
-algebra=input("algebramarks:")
-physics=input("physicsmarks:")
-Geometryint=int(Geometry)
-algebraint=int(algebra)
-physicsint=int(physics)
-#endinitilisation
-#Average marks
-import statistics;
-print("avg is",statistics.mean([Geometryint,algebraint,physicsint]))
+# #initilisation
+# Geometry=input("geometrymarks:")
+# algebra=input("algebramarks:")
+# physics=input("physicsmarks:")
+# Geometryint=int(Geometry)
+# algebraint=int(algebra)
+# physicsint=int(physics)
+# #endinitilisation
+# #Average marks
+# import statistics;
+# print("avg is",statistics.mean([Geometryint,algebraint,physicsint]))
+# #end
+
+ #initilisation
+width=input("width:")
+Height=input("height:")
+widthint=int(width)
+heidhtint=int(Height)
+#end initilisation
+#area logic
+area=(widthint*heidhtint)
+print("area of the place is",area)
 #end
