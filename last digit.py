1fd34764d8d71c83c047aa21b442c77cd6dcfa30
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-10 07:22:12 +0800

    gadget.py

--- last digit.py
+++ last digit.py
@@ -1,31 +1,31 @@
-# #initilisation
-# Number=input("number:")
-# Numberint=int(Number)
-# #endinitilisation
-# #logic
-# lastdigit=(Numberint%10)
-# print("lastdigit is",lastdigit)
-# #end
-
 #initilisation
-numberofhours=input("number of hours worked:")
-numberofhoursint=int(numberofhours)
-hourlyincome=input("income per hour:")
-hourlyincomeint=int(hourlyincome)
-ps4cost=input("ps4cost:")
-ps4costint=int(ps4cost)
-Tvcost=input("Tv cost:")
-Tvcostint=int(Tvcost)
-Gameskin=input("Gameskin:")
-Gameskinint=int(Gameskin)
+Number=input("number:")
+Numberint=int(Number)
 #endinitilisation
 #logic
-totalincome=(numberofhoursint*hourlyincomeint)
-print("Total income is",totalincome)
-Totalcostofgadgets=(ps4costint+Tvcostint+Gameskinint)
-if(totalincome>Totalcostofgadgets):
-    print("I can buy all gadgets")
-else:
-    print("I cannot buy all the items") 
-#end   
+lastdigit=(Numberint%10)
+print("lastdigit is",lastdigit)
+#end
+
+# #initilisation
+# numberofhours=input("number of hours worked:")
+# numberofhoursint=int(numberofhours)
+# hourlyincome=input("income per hour:")
+# hourlyincomeint=int(hourlyincome)
+# ps4cost=input("ps4cost:")
+# ps4costint=int(ps4cost)
+# Tvcost=input("Tv cost:")
+# Tvcostint=int(Tvcost)
+# Gameskin=input("Gameskin:")
+# Gameskinint=int(Gameskin)
+# #endinitilisation
+# #logic
+# totalincome=(numberofhoursint*hourlyincomeint)
+# print("Total income is",totalincome)
+# Totalcostofgadgets=(ps4costint+Tvcostint+Gameskinint)
+# if(totalincome>Totalcostofgadgets):6
+#     print("I can buy all gadgets")
+# else:
+#     print("I cannot buy all the items") 
+# #end   
 
