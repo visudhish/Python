b1beffd55fdbde83f9e10fda38a997b5de57ba96
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-09 13:39:15 +0800

    last digit.py

--- last digit.py
+++ last digit.py
@@ -1,9 +1,30 @@
+# #initilisation
+# Number=input("number:")
+# Numberint=int(Number)
+# #endinitilisation
+# #logic
+# lastdigit=(Numberint%10)
+# print("lastdigit is",lastdigit)
+# #end
+
 #initilisation
-Number=input("number:")
-Numberint=int(Number)
+numberofhours=input("number of hours worked:")
+numberofhoursint=int(numberofhours)
+hourlyincome=input("income per hour:")
+hourlyincomeint=int(hourlyincome)
+ps4cost=input("ps4cost:")
+ps4costint=int(ps4cost)
+Tvcost=input("Tv cost:")
+Tvcostint=int(Tvcost)
+Gameskin=input("Gameskin:")
+Gameskinint=int(Gameskin)
 #endinitilisation
 #logic
-lastdigit=(Numberint%10)
-print("lastdigit is",lastdigit)
-#end
+totalincome=(numberofhoursint*hourlyincomeint)
+Totalcostofgadgets=(ps4costint+Tvcostint+Gameskinint)
+if(totalincome>Totalcostofgadgets):
+    print("I can buy all gadgets")
+else:
+    print("I cannot buy all the items") 
+#end   
 
