3e522f65b92949c54f62c63b7c1046b7ddc4afcd
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-12 00:27:28 +0800

    Beginners guide for finding  factorial

---
+++ Membership.py
@@ -0,0 +1,8 @@
+#Assignment no.2
+#Initilisation
+number=input("number:")
+numberint=int(number)
+#endinitilisation
+#logic
+import math
+print("The factorial of",numberint,"is",math.factorial(numberint))
