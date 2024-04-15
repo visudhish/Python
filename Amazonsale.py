5dd93392e7588359dec38981762cae1dba158eab
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-15 14:12:38 +0800

    finding product cost acc to location

---
+++ Amazonsale.py
@@ -0,0 +1,15 @@
+#Initilisation
+Product cost = input("Cost of the Product:")
+PCint = int(Product cost)
+Location = input("Location name:")
+if(Location = USA):
+pay = 20$+5$
+payint = int(pay)
+    print("You have to pay ",payint,"20$ for the product and 5$ for shipping")
+elseif(Location=Europe):
+pay = 20$+7$
+payint = int(pay)
+    print("You have to pay ",payint,"20$ for the product and 7$ for shipping")
+else(Location = Canada):
+    print("You have to pay ",payint,"20$ for the product and 3$ for shipping")
+#end
