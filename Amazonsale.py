d9cdcb866a0acac2e409421040d31a13c83e8a90
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-17 06:54:21 +0800

    Added new places

--- Amazonsale.py
+++ Amazonsale.py
@@ -3,13 +3,17 @@ Productcost = input("Cost of the Product:")
 PCint = int(Productcost)
 Location = input("Location name:")
 if(Location== "USA"):
-    payindollars=PCint+5
+    payindollars = PCint+5
     payint = int(payindollars)
     print("you have to pay",payint,"in dollars")
 elif(Location =="Europe"):
     payindollars = PCint+7
     payint = int(payindollars)
     print("youhave to pay",payint,"in dollars")
+elif(Location == "Canada"):
+    payindollars = PCint+3
+    payint = int(payindollars)
+    print("you have to pay",payint,"in dollars")
     
 else:
     payindollars=PCint+9
