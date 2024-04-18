2f5b9d51bceaf0724e8056b0dd98793051e3ef88
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-18 23:27:01 +0800

    Basics of for loop

--- sum.py
+++ sum.py
@@ -1,9 +1,20 @@
-# numbers = list(range(1,11))
-# for num in numbers:
-#     print(nu
-#initilisation
-numbers = list(range(12-1))
- 
-numbers.sort(reverse="true")
+#This program displays numbers in Descending order
+numbers = range(10,0,-1)
 for num in numbers:
     print(num)
+#end    
+#This program displays serial numbers from 1-10
+numbers = range(1,11)
+for num in numbers:
+    print(num)
+#end    
+#This program displays odd number
+numbers = range(1,10,2)
+for num in numbers:
+    print(num) 
+#end 
+#This program displays even number
+numbers = range(0,10,2)
+for num in numbers:
+    print(num)       
+#end    
