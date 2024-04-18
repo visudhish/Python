284fb32f526c939997d6502c42d8b864e90d7a96
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-18 15:05:13 +0800

    number list

--- sum.py
+++ sum.py
@@ -1,3 +1,9 @@
-numbers = list(range(1,11))
+# numbers = list(range(1,11))
+# for num in numbers:
+#     print(nu
+#initilisation
+numbers = list(range(12-1))
+ 
+numbers.sort(reverse="true")
 for num in numbers:
     print(num)
