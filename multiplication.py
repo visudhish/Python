64be6c34ab182a1f4b7dfa34e18a6d0fdc2d5a1f
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-20 11:45:15 +0800

    displays multication of given number using for loop

--- multiplication.py
+++ multiplication.py
@@ -1,5 +1,10 @@
+#initilisation
 number = input("Enter a number : ")
 numberint = int(number)
+#endinitilisation
+#logic of multiplication
 for num in range(1,11):
     multiplication = num*numberint
+#end logic of nultication
     print(numberint,"x",num,"=",multiplication)
+#end
