3849686429a90cc33aacb3824a05996e5313f154
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-24 17:58:29 +0800

    this program displays sum of odd number until given number

---
+++ sum.odd.py
@@ -0,0 +1,11 @@
+Numint1 = int(input("enter number1 :"))
+Numint2 = int(input("enter number2 : "))
+sum = 0
+for num in range(Numint1,Numint2,2):
+   sum += num
+   print(sum)
+    
+
+    
+    
+    
