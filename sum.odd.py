e412dc6842a080f8639f6dc26acbd6b989ec1122
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-25 09:15:42 +0800

    This program displays average of odd numbers.

--- sum.odd.py
+++ sum.odd.py
@@ -1,11 +1,16 @@
-Numint1 = int(input("enter number1 :"))
-Numint2 = int(input("enter number2 : "))
+# Numint1 = int(input("enter number1 :"))
+# Numint2 = int(input("enter number2 : "))
+# sum = 0
+# for num in range(Numint1,Numint2,2):
+#    sum += num#    print(sum)
+#This program displays Average of numbers until given number .   
+num = int(input("Enter a number : "))
 sum = 0
-for num in range(Numint1,Numint2,2):
-   sum += num
-   print(sum)
-    
-
-    
+for i in range(1,num+1):
+    sum = i+sum
+Average = sum/num
+print("sum : ",sum)
+print("Average of numbers until given number is",Average)
+#end program
     
     
