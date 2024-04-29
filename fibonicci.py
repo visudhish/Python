6a7e301635265ee86bee663acd71c5ed6b5f2055
VIswani Gudla <gudlaviswani@gmail.com>
2024-04-29 23:38:34 +0800

    This program displays fibonicci series

--- fibonicci.py
+++ fibonicci.py
@@ -1,5 +1,14 @@
-previousnumber=0
-for loopnumber in range(20):
-    sum = previousnumber+loopnumber
-    number = loopnumber+sum
-    print(number)
+#This program Displays fibonicci  series until given range
+nminustwo=0
+nminusone= 1
+print (nminustwo)
+print (nminusone)
+for loopnumber in range(1,10):
+    n= nminusone+nminustwo
+    print(n)
+    nminustwo=nminusone
+    nminusone=n
+#end program
+   
+
+    
