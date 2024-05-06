841a52d5b10a8bb19ac17aa1d2cf797fe3691b8a
VIswani Gudla <gudlaviswani@gmail.com>
2024-05-06 12:02:55 +0800

    This program displays name using while loop

---
+++ whileloop4.py
@@ -0,0 +1,13 @@
+#This program displays given name for given number of times
+#input name
+Name = input("Enter a name : ")
+#input numberoftimes name to be printed
+Numberoftimes = int(input("number of times name to be printed : "))
+#starting numbercount
+Numbercount = 0
+#print name num of times using while loop
+while Numbercount <= Numberoftimes:
+    print(Name)
+    Numbercount = Numbercount+1
+#end program
+
