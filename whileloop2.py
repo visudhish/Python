05df5ae4d50e65e3cb1ff233f692ff85b6687953
VIswani Gudla <gudlaviswani@gmail.com>
2024-05-06 11:12:54 +0800

    This program displays numbers1=10

---
+++ whileloop2.py
@@ -0,0 +1,6 @@
+number = 0
+while number<=10:
+    number = int(input("Enter a number : "))
+    print(number)
+ 
+print(number, "-","you have entered a number more than 10")
---
+++ whileloop3.py
@@ -0,0 +1,4 @@
+number = 0
+while number%2==0:
+    number = int(input("Enter a number :"))
+print(number,"-","you have entered an odd number")
