7cd3948d5f7e9820d99f096fd59dd61dc9e73f5a
VIswani Gudla <gudlaviswani@gmail.com>
2024-05-06 20:24:15 +0800

    displays even number until you enter an odd number

--- whileloop3.py
+++ whileloop3.py
@@ -1,4 +1,6 @@
+#This program displays even number until you enter an odd number
 number = 0
 while number%2==0:
     number = int(input("Enter a number :"))
 print(number,"-","you have entered an odd number")
+#end program
