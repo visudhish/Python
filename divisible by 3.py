3579fd6773c5c39c76381c444917d805fd3e68b5
VIswani Gudla <gudlaviswani@gmail.com>
2024-07-26 13:25:06 +0800

    this program displays the number divisible by 3 in between 1-100

---
+++ divisible by 3.py
@@ -0,0 +1,6 @@
+#This program diszplays given number is divisible by 3 or not
+number = {}
+for number in range(1,100,1):
+    if number % 3 == 0:
+        print(number,"is divisible by 3")
+        
---
+++ two sum.py
@@ -0,0 +1,12 @@
+# Initialize a variable
+count = 1
+
+# Set up the while loop
+while count <= 5:
+    # Print the current value of count
+    print(count)
+    
+    # Increment the count by 1
+    count += 1
+
+print("Loop has finished!")
