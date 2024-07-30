db87b56d9457b4168efeae90ac461740b36af0a6
VIswani Gudla <gudlaviswani@gmail.com>
2024-07-30 19:38:59 +0800

    Today Changes

---
+++ lists.py
@@ -0,0 +1,66 @@
+# /*#num = 4.3
+# data = [num,"word",True]
+# print(data)
+# key = "lock"
+# data = [5,key,[34,"hello"],False]
+# print(data)
+# print(data[2][2])
+
+# data = [5,10,15,20]
+# data[0]= 20
+# print(data)
+# a = [5,10]
+# b = a
+# print(id(a))
+
+
+
+# data = [5,10,15,20]
+# data[0] = 50
+# data_copy = data[:]
+# print(data_copy)
+
+
+
+# #This program displays the given list
+# days = ["sun","mon","tue"]
+# print(days)
+# #end program
+
+# month = ["jan","feb","march","april","may"]
+# week = ["sun","mon","tue","wed"]
+# print(month[0])
+# print(month[4])
+# print(week[0])
+# print(week[3])
+
+
+# numbers = [1,2,3,4,5,6]
+# print(len(numbers))
+# numbers = [3,4,5,6,7,8,9]
+# print("list before swap",numbers)
+# newnumber = numbers[0]
+# numbers[0] = numbers[6]
+# numbers[6] = newnumber
+# print("list after swap",numbers)
+
+
+list = [9,2,8,1,5,76]
+max = list[0]
+for num in list:
+    if num > max:
+        max = num
+print(max)
+
+# list = [9,2,8,1,5,76]
+# min=list[0]
+# for num in list:
+#     print ("I am in loop ", num)
+#     if num < min:
+#         min = num
+#         print ("   I came to if and changed min value ",min)
+# print ("smallest num among list is ", min)
+
+
+
+
